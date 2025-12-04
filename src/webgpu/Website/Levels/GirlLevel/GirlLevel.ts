7affcdda4a2d28d1079195de18a8dbcd5f2053f9
Kris Temmerman <kris@neuroproductions.be>
2025-12-04 12:03:17 +0100

    fairyUpdate

--- src/webgpu/Website/Levels/GirlLevel/GirlLevel.ts
+++ src/webgpu/Website/Levels/GirlLevel/GirlLevel.ts
@@ -12,11 +12,13 @@ import SceneObject3D from "../../../data/SceneObject3D.ts";
 import { HitTrigger } from "../../../data/HitTriggers.ts";
 import gsap from "gsap";
 import LevelHandler from "../LevelHandler.ts";
+import FaceHandler from "../../handlers/FaceHandler.ts";
 
 
 export class GirlLevel extends PlatformLevel {
-    private startPos: number = -5;
+    private startPos: number = -10;
     girl!: SceneObject3D;
+    charFaceHandler!: FaceHandler;
 
 
 
@@ -27,13 +29,15 @@ export class GirlLevel extends PlatformLevel {
         LoadHandler.startLoading()
         LoadHandler.startLoading()
 
-
+        LoadHandler.startLoading()
 
         SceneHandler.setScene("316d99ad-bd50-49e3").then(() => {
             SceneHandler.addScene("1234").then(() => {
                 LoadHandler.stopLoading()
             });
-
+            SceneHandler.addScene("e5e28257-5e4f-4630").then(() => {
+                LoadHandler.stopLoading()
+            });
             LoadHandler.stopLoading()
         })
 
@@ -50,7 +54,7 @@ export class GirlLevel extends PlatformLevel {
 
         GameModel.gameRenderer.setModels(SceneHandler.allModels)
         GameModel.gameRenderer.addModel(this.characterController.cloudParticles.particlesModel)
-
+        this.isConversation = true
 
         let charRoot = SceneHandler.getSceneObject("charRoot");
         charRoot.x = this.startPos
@@ -60,14 +64,21 @@ export class GirlLevel extends PlatformLevel {
         this.isConversation = true
         this.characterController.gotoAndIdle(new Vector3(this.startPos + 0.5, 0, 0), 1, () => { this.isConversation = false })
 
-        this.girl = SceneHandler.getSceneObject("girl");
-
+        this.girl = SceneHandler.getSceneObject("rootFairy");
+        this.girl.x = 5
+        this.girl.y = 0.6
+        this.girl.ry = -0.3
+        this.girl.setScaler(1.2)
         GameModel.gameCamera.setMinMaxX(this.startPos, this.girl.x + 2 + 5)
 
 
         GameModel.gameCamera.setForCharPos(new Vector3(this.startPos, 0, 0))
 
-
+        this.charFaceHandler = new FaceHandler(charRoot)
+        this.charFaceHandler.setState("default")
+    }
+    onUI(): void {
+        this.charFaceHandler?.onUI()
     }
     resolveHitTrigger(f: SceneObject3D) {
         if (!super.resolveHitTrigger(f)) {
@@ -77,6 +88,7 @@ export class GirlLevel extends PlatformLevel {
 
 
             if (f.hitTriggerItem == HitTrigger.GIRL) {
+
                 f.triggerIsEnabled = false;
                 GameModel.conversationHandler.replaceMap.set("numCoins", GameModel.coinHandler.numCoins + "")
                 GameModel.conversationHandler.replaceMap.set("numFishsticks", GameModel.fishstickHandler.numFishsticks + "")
@@ -84,9 +96,10 @@ export class GirlLevel extends PlatformLevel {
                 this.isConversation = true
                 this.characterController.gotoAndIdle(new Vector3(this.girl.x - 1.0, 0, 0), 1, () => {
                     let target = new Vector3(this.girl.x - 0.5, 0.6, 0)
-                    GameModel.gameCamera.TweenToLockedView(target, target.clone().add([0, 0, 2.3]))
+                    GameModel.gameCamera.TweenToLockedView(target, target.clone().add([0, 0, 1.9]))
 
                     gsap.delayedCall(0.5, () => {
+                        this.charFaceHandler.setState("lookGirl")
                         GameModel.conversationHandler.startConversation("girl")
 
                         GameModel.conversationHandler.doneCallBack = () => {
