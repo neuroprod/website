e7f6bbc154d4cdf1065084f97691eb10746de048
Kris Temmerman <kris@neuroproductions.be>
2026-01-22 12:25:34 +0100

    test

--- src/webgpu/Website/UI2D/JoyStick.ts
+++ src/webgpu/Website/UI2D/JoyStick.ts
@@ -5,7 +5,7 @@ import Object2D from "../../lib/twoD/Object2D";
 import Sprite from "../../lib/twoD/Sprite";
 import GameModel from "../GameModel";
 import GameInput from "../GameInput";
-
+import { gsap } from "gsap";
 export default class JoyStick {
     joystickRoot: Object2D;
     renderer: Renderer;
@@ -29,24 +29,24 @@ export default class JoyStick {
 
 
         this.joyStickBack.mouseDown = () => {
-            
+
             this.down = true;
             let pos = GameModel.mouseListener.mousePos.clone()
             pos.scale(1 / this.renderer.pixelRatio)
             this.setAllPos(pos.x, pos.y)
-            
+            gsap.killTweensOf(this.joyStick)
 
         }
         this.joyStickBack.mouseUp = () => {
-        
+
             this.down = false;
             GameInput.setJoystick(0, 0)
-          
 
 
+            gsap.to(this.joyStick, { x: this.joyStickBack.x, y: this.joyStickBack.y, duration: 0.5, ease: "elastic.out(1, 0.5)" })
 
         }
-     
+
         this.setAllPos(100, 300)
 
     }
@@ -61,7 +61,7 @@ export default class JoyStick {
 
             let pos = GameModel.mouseListener.mousePos.clone()
             pos.scale(1 / this.renderer.pixelRatio)
-           
+
             this.joyStick.x = pos.x;
             this.joyStick.y = pos.y;
 
@@ -69,21 +69,21 @@ export default class JoyStick {
             this.moveVec.subtract([this.joyStickBack.x, this.joyStickBack.y])
 
 
-        
+
             let moveLen = this.moveVec.len()
             if (moveLen > 50) {
                 this.moveVec.normalize()
-                this.moveVec.scale(moveLen-50)
-               this.joyStickBack.x += this.moveVec.x
-               this.joyStickBack.y += this.moveVec.y
+                this.moveVec.scale(moveLen - 50)
+                this.joyStickBack.x += this.moveVec.x
+                this.joyStickBack.y += this.moveVec.y
                 this.moveVec.set(this.joyStick.x, this.joyStick.y)
-            this.moveVec.subtract([this.joyStickBack.x, this.joyStickBack.y])
+                this.moveVec.subtract([this.joyStickBack.x, this.joyStickBack.y])
 
             }
-            
-            this.moveVec.scale(1/50);
+
+            this.moveVec.scale(1 / 50);
             GameInput.setJoystick(this.moveVec.x, this.moveVec.y)
-       
+
 
         }
     }
